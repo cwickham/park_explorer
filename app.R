d7c7a8819448a21aca589118e840e4a1b726ac1f
Charlotte Wickham <cwickham@gmail.com>
2020-06-10 12:36:26 -0700

    Demonstrate table output to check data is available.

--- app.R
+++ app.R
@@ -7,11 +7,14 @@ monthly_visits <- read_rds("data/monthly_visits.rds")
 
 ui <- fluidPage(
   textInput("selected_park", "Park"),
-  textOutput("park_name")
+  textOutput("park_name"),
+  tableOutput("data")
 )
 
 server <- function(input, output, session) {
   output$park_name <- renderText(input$selected_park)
+  
+  output$data <- renderTable(head(monthly_visits))
 }
 
 shinyApp(ui, server)
