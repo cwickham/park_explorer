9cba2d9a9cea5ede621c70bc5c075b951059846b
Charlotte Wickham <cwickham@gmail.com>
2020-06-16 12:48:31 -0700

    Move plotting code into functions

--- app.R
+++ app.R
@@ -48,24 +48,13 @@ server <- function(input, output, session) {
 
   output$annual_plot <- renderPlot({
     annual_data() %>% 
-      ggplot(aes(year, recreation_visits)) +
-      geom_point(data = ~ filter(., year == 2019)) +
-      geom_line() +
-      scale_y_continuous(labels = scales::label_comma()) +
-      labs(x = "", y = "Visits")
+      plot_annual()
   })
 
   output$monthly_plot <- renderPlot({
     monthly_visits %>% 
       filter(park_name == input$selected_park) %>% 
-      ggplot(aes(month, recreation_visits_proportion)) +
-      geom_line(aes(group = year), alpha = 0.2) +
-      geom_line(data = ~ filter(.x, year == 2019)) +
-      stat_summary(fun = mean, 
-        geom = "line", color = "#325D88", size = 1.5) +
-      scale_x_continuous(breaks = 1:12, labels = month.abb) +
-      scale_y_continuous(labels = scales::label_percent()) +
-      labs(x = "", y = "Of Annual Visits")
+      plot_monthly() 
   })
   
 }
