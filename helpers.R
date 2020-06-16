32eaf42bbe0b239414f8b9c39fce013138fb8405
Charlotte Wickham <cwickham@gmail.com>
2020-06-16 13:22:16 -0700

    Make display of average optional in plot_monthly

--- helpers.R
+++ helpers.R
@@ -19,14 +19,17 @@ plot_annual <- function(annual_data){
 }
 
 # Takes monthly data and produces a plot
-plot_monthly <- function(monthly_data){
-  monthly_data %>% 
+plot_monthly <- function(monthly_data, display_average = TRUE){
+  p <- monthly_data %>% 
     ggplot(aes(month, recreation_visits_proportion)) +
     geom_line(aes(group = year), alpha = 0.2) +
     geom_line(data = ~ filter(.x, year == 2019)) +
-    stat_summary(fun = mean, 
-      geom = "line", color = "#325D88", size = 1.5) +
     scale_x_continuous(breaks = 1:12, labels = month.abb) +
     scale_y_continuous(labels = scales::label_percent()) +
     labs(x = "", y = "Of Annual Visits")
+  if(display_average) {
+    p <- p + stat_summary(fun = mean, 
+      geom = "line", color = "#325D88", size = 1.5) 
+  }
+  p
 }
